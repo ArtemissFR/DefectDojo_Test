40cecab3e965c92125acd2dcbeeba2cf3b12531b
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-10-01 15:58:53 +0200

    Update nmap_scan.sh

--- Nmap+DefectDojo/nmap_scan.sh
+++ Nmap+DefectDojo/nmap_scan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Exécute Nmap
-nmap -v -O -p- 192.168.1.0/24 -oX nmap_scan_report.xml
+nmap -v -p 1-1000 192.168.1.0/24 -oX nmap_scan_report.xml # > Port 1 to 1000 scan
+#nmap -v -O -p- 192.168.1.0/24 -oX nmap_scan_report.xml # > Full Port Scan
 
 # Enregistre la date d'exécution pour référence
 echo "Nmap scan exécuté à $(date)" >> ~/nmap_scan.log
